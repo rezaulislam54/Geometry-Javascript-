d3d6f66d069c5d9c33196f003d27070e2b25c84e
rezaulislam54 <rezaulislam5490@gmail.com>
2024-08-20 13:12:50 +0600

    some change

--- index.js
+++ index.js
@@ -12,9 +12,42 @@ function geteRectangleArea(){
     const height = geteInputValu('rectangleheight');
     const area = base * height;
     setAreaValu('rectanglevaluset', area);
+    // document.getElementById('rectanglebase').value = '';
 }
 
-// 
+//  Parallelogram Area
+function geteParallelogramArea(){
+    const base = geteInputValu('ParallelogramArea');
+    const height = geteInputValu('Parallelogramheight');
+    const Area = base * height;
+    setAreaValu('Parallelogramdisplay', Area);
+}
+
+// Rhombus Area 
+
+function geteRombosAreaValu(){
+   const b = geteInputValu('parallelogrambase');
+   const h = geteInputValu('parallelogramheight');
+   const a = 0.5 * b * h;
+    setAreaValu('rhombusdisplay', a);
+}
+
+// Pentagon Area Calculation
+
+function getePentagonArea(){
+    const base = geteInputValu('pentagonbase');
+    const height = geteInputValu('pentagonheight');
+    const a = 0.5 * base * height;
+    setAreaValu('pentagondisplay', a);
+}
+
+// Ellipse Area Calculation
+function geteEllipseArea(){
+    const base = geteInputValu('ellipsebase');
+    const height = geteInputValu('ellipseheight');
+    const a = 0.5 * base * height;
+    setAreaValu('ellipsedisplay', a);
+}
 
 // common Function
 
